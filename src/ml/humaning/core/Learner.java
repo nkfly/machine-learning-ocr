ea69688484cd187eca18797010858d1fd282ee25
nkfly <nkflyfly@gmail.com>
2013-12-11 16:37:52 +0800

    use cross validation to find optimal K

--- src/ml/humaning/core/Learner.java
+++ src/ml/humaning/core/Learner.java
@@ -1,5 +1,8 @@
 package ml.humaning.core;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+
 
 
 public class Learner {
@@ -16,9 +19,25 @@ public class Learner {
 			if(argv.length < 4){
 				System.exit(-1);
 			}
-			try {
+			try {// argv[0] : -knn, argv[1] : trainFile , argv[2] : testFile , argv[3] outputFile
 				KNN knn = new KNN(argv[1]);
-				System.out.println(knn.getValidationError(3, 10, 1));
+				int optimalK = 0;
+				double optimalCrossValidationError = Double.MAX_VALUE;
+				BufferedWriter writer = new BufferedWriter(new FileWriter("knn_validation"));
+				writer.write("k\tcrossValidationError\n");
+				for(int i = 1;i <= 100;i++){
+					double crossValidationError = 0.0;
+					for(int j = 1;j <= 10;j++){// perform the 10-fold validation
+						crossValidationError += knn.getValidationError(i, 10, j);
+					}
+					crossValidationError /= 10;
+					if(crossValidationError < optimalCrossValidationError){
+						optimalCrossValidationError = crossValidationError;
+						optimalK = i;
+					}
+					writer.write(i+"\t"+crossValidationError);					
+				}
+				writer.write("optimal: "+optimalK+"\t"+optimalCrossValidationError);
 
 
 
