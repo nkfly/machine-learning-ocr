b4eef74fec7965e51f757f2d823506cdb3d4bc7a
Ray Shih <mnf.shih@gmail.com>
2013-12-23 23:45:49 +0800

    remove redundant jar

--- .vimrc
+++ .vimrc
@@ -1,2 +1,3 @@
 let g:syntastic_java_javac_classpath="./src\n"
-let g:syntastic_java_javac_classpath.="./libs/commons-cli-1.2/commons-cli-1.2.jar"
+let g:syntastic_java_javac_classpath.="./libs/commons-cli-1.2/commons-cli-1.2.jar\n"
+let g:syntastic_java_javac_classpath.="./libs/weka.jar"
--- src/ml/humaning/algorithm/ANN.java
+++ src/ml/humaning/algorithm/ANN.java
@@ -12,33 +12,33 @@ public class ANN {
 	private Instances data;
 	private MultilayerPerceptron multilayerPerceptron;
 	public ANN() {
-		
+
 	}
-	
+
 	public void train(String trainFile) throws Exception{
 		LibSVMLoader libsvmLoader = new LibSVMLoader();
 		libsvmLoader.setSource(new File(trainFile));
 		data = libsvmLoader.getDataSet();
-		
+
 		multilayerPerceptron = new MultilayerPerceptron();
-		multilayerPerceptron.buildClassifier(data);		 
-		
+		multilayerPerceptron.buildClassifier(data);
+
 	}
-	
+
 	public void predict(String testFile, String outputFile) throws Exception{
 		LibSVMLoader libsvmLoader = new LibSVMLoader();
 		libsvmLoader.setSource(new File(testFile));
 		Instances test = libsvmLoader.getDataSet();
-		
+
 		BufferedWriter bw = new BufferedWriter(new FileWriter(outputFile));
-		
+
 		for (int i = 0; i < test.numInstances(); i++) {
 			double pred = multilayerPerceptron.classifyInstance(test.instance(i));
 			bw.write(test.classAttribute().value((int) pred)+"\n");
 		}
-		
-		bw.close();		
-		
+
+		bw.close();
+
 	}
 
 }
