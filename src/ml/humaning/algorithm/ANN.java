8f93600280f396ba9976bc4491c7b9e458ffbcfb
nkfly <nkflyfly@gmail.com>
2013-12-23 22:55:58 +0800

    before test multilayerperception prediction

--- src/ml/humaning/algorithm/ANN.java
+++ src/ml/humaning/algorithm/ANN.java
@@ -3,17 +3,43 @@ package ml.humaning.algorithm;
 import java.io.File;
 import java.io.IOException;
 
+import weka.classifiers.Classifier;
+import weka.classifiers.functions.MultilayerPerceptron;
 import weka.core.Instances;
 import weka.core.converters.LibSVMLoader;
 
 public class ANN {
 	private Instances data;
-	public ANN(String inputFile) throws IOException{
+	private MultilayerPerceptron multilayerPerceptron;
+	public ANN() {
+		
+	}
+	
+	public void train(String trainFile) throws Exception{
 		LibSVMLoader libsvmLoader = new LibSVMLoader();
-		libsvmLoader.setSource(new File(inputFile));
+		libsvmLoader.setSource(new File(trainFile));
 		data = libsvmLoader.getDataSet();
 		
-		System.out.println(data.classIndex());
+		System.out.println(data.firstInstance().classValue());
+		
+		multilayerPerceptron = new MultilayerPerceptron();
+		multilayerPerceptron.buildClassifier(data);		 
+		
+	}
+	
+	public void predict(String testFile) throws Exception{
+		LibSVMLoader libsvmLoader = new LibSVMLoader();
+		libsvmLoader.setSource(new File(testFile));
+		Instances test = libsvmLoader.getDataSet();
+		
+		for (int i = 0; i < test.numInstances(); i++) {
+			   double pred = multilayerPerceptron.classifyInstance(test.instance(i));
+			   System.out.println(test.classAttribute().value((int) pred));
+			 }
+		
+		
+		multilayerPerceptron = new MultilayerPerceptron();
+		multilayerPerceptron.buildClassifier(data);		 
 		
 	}
 
