f1d1370865f4d12401dcfb67727e78d6a30ae9c2
Ray Shih <mnf.shih@gmail.com>
2014-01-04 10:47:04 +0800

    refactor interface for cross validation

--- src/ml/humaning/algorithm/Algorithm.java
+++ src/ml/humaning/algorithm/Algorithm.java
@@ -21,6 +21,7 @@ public abstract class Algorithm implements Runner {
 	Instances trainData;
 	Instances testData;
 	String outputPath;
+	String runMode = "normal";
 	Options options;
 	CommandLine line;
 
@@ -28,12 +29,14 @@ public abstract class Algorithm implements Runner {
 		if (options == null) options = new Options();
 
 		Option trainFile = new Option("tr", "train-file", true, "train file");
-		Option testFile = new Option("te", "test-file", true, "test file");
-		Option output = new Option("o", "output", true, "output file");
+		Option testFile  = new Option("te", "test-file", true, "test file");
+		Option output    = new Option("o", "output", true, "output file");
+		Option mode   	 = new Option("m", "mode", true, "run mode: cv, lm");
 
 		options.addOption(trainFile);
 		options.addOption(testFile);
 		options.addOption(output);
+		options.addOption(mode);
 	}
 
 	public boolean parseArgv(String [] argv, Options options) throws Exception {
@@ -47,15 +50,37 @@ public abstract class Algorithm implements Runner {
 		CommandLineParser parser = new GnuParser();
 		line = parser.parse(options, argv, true);
 
-		if (!line.hasOption("train-file") ||
-				!line.hasOption("test-file") ||
-				!line.hasOption("output")) {
-			return false;
+		if (line.hasOption("mode")) {
+			this.runMode = line.getOptionValue("mode");
 		}
 
-		this.trainData = Reader.readData(line.getOptionValue("train-file"));
-		this.testData = Reader.readData(line.getOptionValue("test-file"));
-		this.outputPath = line.getOptionValue("output");
+		if ("normal".equals(this.runMode)) {
+			if (!line.hasOption("train-file") ||
+					!line.hasOption("test-file") ||
+					!line.hasOption("output")) {
+				return false;
+			}
+
+			this.trainData = Reader.readData(line.getOptionValue("train-file"));
+			this.testData = Reader.readData(line.getOptionValue("test-file"));
+			this.outputPath = line.getOptionValue("output");
+
+		} else if ("lm".equals(this.runMode)) {
+			if (!line.hasOption("test-file") ||
+					!line.hasOption("output")) {
+				return false;
+			}
+
+			this.testData = Reader.readData(line.getOptionValue("test-file"));
+			this.outputPath = line.getOptionValue("output");
+
+		} else if ("cv".equals(this.runMode)) {
+			if (!line.hasOption("train-file")) {
+				return false;
+			}
+
+			this.trainData = Reader.readData(line.getOptionValue("train-file"));
+		}
 
 		return true;
 	}
@@ -92,11 +117,38 @@ public abstract class Algorithm implements Runner {
 		Logger.log("DONE");
 	}
 
-	public void run() throws Exception {
+	public void runPredictWithTrainData() throws Exception {
 		this.train(trainData);
 		ArrayList<Integer> results = this.predict(testData);
 
 		saveModel();
 		writeResult(results);
+	}
+
+	public void runPredictWithLoadModel() throws Exception {
+		this.loadModel();
+
+		ArrayList<Integer> results = this.predict(testData);
+		writeResult(results);
+	}
+
+	public void runCrossValidation() throws Exception {
+
+	}
+
+	public void run() throws Exception {
+		if ("normal".equals(this.runMode)) {
+			Logger.log("Run Training -> Predict");
+
+			this.runPredictWithTrainData();
+		} else if ("lm".equals(this.runMode)) {
+			Logger.log("Run Load model -> Predict");
+
+			this.runPredictWithLoadModel();
+		} else if ("cv".equals(this.runMode)) {
+			Logger.log("Run Cross Validation");
+
+			this.runCrossValidation();
+		}
 	}
 }
